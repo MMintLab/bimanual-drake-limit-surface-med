e12a0c17b70fa9f1ab8912b310132a7be937fdeb
An <dangthanhan507@gmail.com>
2024-09-16 19:56:26 -0400

    Added updated load data.

--- src/hardware_kuka/load_data.py
+++ src/hardware_kuka/load_data.py
@@ -3,9 +3,9 @@ import os
 
 # given a folder and object, generate MSE error for each path
 if __name__ == '__main__':
-    is_naive = False
+    is_naive = True
     is_closed_loop = True
-    object = "circle"
+    object = "square"
     even = True # should not be false
     foldername = f"data/{'naive' if is_naive else 'algo'}/{'closed_loop' if is_closed_loop else 'open_loop'}/{object}/{'even' if even else 'uneven'}/"
     if not os.path.exists(foldername):
@@ -23,15 +23,24 @@ if __name__ == '__main__':
         qgoal_thanos = data[2]
         qgoal_medusa = data[3]
         
+        qthanos[2] *= 180/np.pi
+        qgoal_thanos[2] *= 180/np.pi
+        qmedusa[2] *= -180/np.pi
+        qgoal_medusa[2] *= 180/np.pi
+        
         thanos_translation_error = np.linalg.norm(qthanos[:2] - qgoal_thanos[:2])
-        thanos_rotation_error = np.abs(qthanos[2] - qgoal_thanos[2])
+        thanos_rotation_error = np.abs(qthanos[2] - qgoal_thanos[2]) % 360
+        if thanos_rotation_error > 180:
+            thanos_rotation_error = 360 - thanos_rotation_error
+            
         #wrap rotation error between 0 and np.pi
-        thanos_rotation_error = thanos_rotation_error % np.pi
         
         medusa_translation_error = np.linalg.norm(qmedusa[:2] - qgoal_medusa[:2])
-        medusa_rotation_error = np.abs(qmedusa[2] - qgoal_medusa[2])
-        #wrap rotation error between 0 and np.pi
-        medusa_rotation_error = medusa_rotation_error % np.pi
+        
+        medusa_rotation_error = np.abs(qmedusa[2] - qgoal_medusa[2]) % 360
+        if medusa_rotation_error > 180:
+            medusa_rotation_error = 360 - medusa_rotation_error
+        
         
         thanos_translation_errors.append(thanos_translation_error)
         thanos_rotation_errors.append(thanos_rotation_error)
@@ -57,12 +66,15 @@ if __name__ == '__main__':
     
     print(f"Results for {'closed loop' if is_closed_loop else 'open loop'}, {'naive' if is_naive else 'algo'}, {object}, {'even' if even else 'uneven'}")
     print(f"Thanos translation RMSE: {thanos_translation_rmse * 1000}")
-    print(f"Thanos rotation RMSE: {thanos_rotation_rmse * 180 / np.pi}")
-    print(f"Medusa translation RMSE: {medusa_translation_rmse * 1000}")
-    print(f"Medusa rotation RMSE: {medusa_rotation_rmse * 180 / np.pi}")
-    #get standard deviation
     print(f"Thanos translation stdev: {thanos_translation_stdev * 1000}")
-    print(f"Thanos rotation stdev: {thanos_rotation_stdev * 180 / np.pi}")
+    print()
+    print(f"Thanos rotation RMSE: {thanos_rotation_rmse}")
+    print(f"Thanos rotation stdev: {thanos_rotation_stdev}")
+    print()
+    print(f"Medusa translation RMSE: {medusa_translation_rmse * 1000}")
     print(f"Medusa translation stdev: {medusa_translation_stdev * 1000}")
-    print(f"Medusa rotation stdev: {medusa_rotation_stdev * 180 / np.pi}")
+    print()
+    print(f"Medusa rotation RMSE: {medusa_rotation_rmse}")
+    print(f"Medusa rotation stdev: {medusa_rotation_stdev}")
+    #get standard deviation
     pass
